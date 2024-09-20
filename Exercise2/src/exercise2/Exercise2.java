92505360da8501663b95912e9d3c61c28b888cc7
roman.saborido <roman.saborido@106-28.info2>
2024-09-20 14:22:22 +0200

    Ejercicio 2 terminado

---
+++ Exercise2/src/exercise2/Exercise2.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+			System.out.println("Hello, How are you?");
+			System.out.println("Fine thanks.");
+
+	}
+
+}
